649916b269cb2f4a0b79dd3a95bf0739eb702e46
seungjae-yu <kimug2145@gmail.com>
2022-02-25 17:04:57 +0900

    --all.sh master

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy with vuepress'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:seungjae_yu/TIL.git master:gh-pages
+git push -f git@github.com:seungjae-yu/TIL.git master:gh-pages
 
 cd -
