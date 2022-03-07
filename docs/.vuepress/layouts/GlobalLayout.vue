440740f29163c409dd18e960b2d0c9625955d734
seungjae-yu <kimug2145@gmail.com>
2022-03-07 18:15:12 +0900

    댓글남기기 재수정

--- docs/.vuepress/layouts/GlobalLayout.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div id="global-layout">
-        <component :is="$page.path ? 'Layout' : 'NotFound'" />
-        <Comment />
-    </div>
-</template>
---
+++ docs/.vuepress/theme/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+    <ParentLayout>
+        <Comment />
+    </ParentLayout>
+</template>
+
+<script>
+import ParentLayout from "@parent-theme/layouts/Layout.vue";
+import Comment from "../components/Comment";
+export default {
+    components: {
+        ParentLayout,
+        Comment,
+    },
+};
+</script>
