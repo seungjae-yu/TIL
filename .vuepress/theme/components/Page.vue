6e9463087add8ab788bc5f130c9f3e289acb9990
seungjae-yu <kimug2145@gmail.com>
2022-03-24 15:39:24 +0900

    add hits count

--- .vuepress/theme/components/Page.vue
+++ .vuepress/theme/components/Page.vue
@@ -2,6 +2,10 @@
   <main class="page">
     <slot name="top" />
 
+    <div class="hit">
+      <img :src="hitUrl" alt="Hits bn" />
+    </div>
+
     <Content class="theme-default-content" />
     <PageEdit />
 
@@ -17,9 +21,21 @@ import PageEdit from '@theme/components/PageEdit.vue'
 import PageNav from '@theme/components/PageNav.vue'
 import Comment from '@theme/components/Comment.vue'
 
+const baseURL = `https://seungjae-yu.github.io/TIL/`;
+
 export default {
   components: { PageEdit, PageNav , Comment},
-  props: ['sidebarItems']
+  props: ['sidebarItems'],
+  data () {
+    return {
+      hitUrl: `https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=${baseURL}${this.$route.path}&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false`
+    }
+  },
+  watch : {
+    ['$route.path'] () {
+      this.hitUrl = `https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=${baseURL}${this.$route.path}&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false`;
+    }
+  }
 }
 </script>
 
